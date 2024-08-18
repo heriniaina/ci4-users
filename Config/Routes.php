3577ea7954bb156f2931405700a4b9b19576e951
Hery <heriniaina.eugene@gmail.com>
2024-08-18 16:39:17 +0200

    Adjust permission to manage users

--- Config/Routes.php
+++ Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
- $routes->group('users', ['namespace' => '\Solaitra\Users\Controllers', 'filter' => 'session'], function($routes) {
+ $routes->group('admin/users', ['namespace' => '\Solaitra\Users\Controllers', 'filter' => 'permission:users.manage'], function($routes) {
     $routes->get('/', 'UserController::index');
     $routes->match(['POST', 'GET'], 'create', 'UserController::create');
     $routes->match(['POST', 'GET'] , 'edit/(:num)', 'UserController::edit/$1');
